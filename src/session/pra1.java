cec6b9650deff84ae39e31f3ff3355268e1929de
qhdudedi <boyoung035@gmail.com>
2023-04-03 16:57:46 +0900

    first commit

---
+++ src/session/pra1.java
@@ -0,0 +1,19 @@
+package session;
+
+public class pra1 {
+    public static void main(String[] args) {
+    int kor = 92;
+    int eng = 80;
+    int math = 96;
+
+    //3으로 나누는 것과 3.0으로 나누는 것과 자료형 타입이 달라진다.
+    double avg = (kor + eng + math)/3.0;
+    double avg2 = (kor + eng + math)/3;
+
+    System.out.println("평균점수(실수형) = " + avg);      // 출력 값 89.33333333
+    System.out.println("평균점수(정수형) = " + (int)avg); // 출력 값 89
+    System.out.println(avg2); // 출력 값 89.0
+
+}
+
+}
---
+++ src/session/pra2.java
@@ -0,0 +1,25 @@
+package session;
+
+public class pra2 {
+    public static void main(String[] args) {
+        int a=169, b=4;
+        char op='*';  //op의 기호에 따라서 값이 달라지도록
+
+        int res1 = 0;
+        double res2 = 0.0;
+
+        switch(op){
+            case '+' : res1 = a + b; break;
+            case '-' : res1 = a - b; break;
+            case '*' : res1 = a * b; break;
+            case '/' : res2 = (double) a / b; break;
+            case '%' : res1 = a % b; break;
+        }
+
+        if(op=='/'){
+            System.out.println(a+" 나누기 "+b+"는 "+res2);
+        }else{
+            System.out.println(a+" "+op+" "+b+" = "+res1);
+        }
+    }
+}

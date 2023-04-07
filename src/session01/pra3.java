39d74d8a18ecdeebdeca3a859433f9376b39ec17
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-04-08 02:54:36 +0900

    pra3

--- src/session01/pra3.java
+++ src/session01/pra3.java
@@ -1,21 +1,22 @@
 package session01;
 
 public class pra3 {
-    public static void main(String[] args){
-        int sum =0;
-        for(int i=1; i<=100; i++){
-            sum = sum+i;
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum = sum + i;
         }
-        System.out.println("1~100까지의 합 : "+ sum);
+        System.out.println("1~100까지의 합 : " + sum);
     }
-    /**
-    public static void main(String[] args){
-        int sum =0;
-        while(i<=100){
+}
+/**
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+        while (i <= 100) {
             sum += i;
             i++;
-     }
-        System.out.println("1~%d까지의 합 : %d", i-1,sum);
+        }
+        System.out.printf("1~%d까지의 합 : %d", i - 1, sum);
     }
-    **/
-}
+ **/

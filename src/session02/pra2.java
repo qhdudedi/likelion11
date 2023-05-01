d7ae93dedaaae1b93c8d20203e15615951264516
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-05-02 00:06:47 +0900

    Update pra2.java

--- src/session02/pra2.java
+++ src/session02/pra2.java
@@ -1,18 +1,1 @@
-package session02;
 
-import java.util.Scanner;
-
-public class pra2 {
-    public static void findEvenOdd(int num){
-        if(num % 2 == 0)
-            System.out.println(num + " is Even(짝수)");
-        else
-            System.out.println(num + " is Odd(홀수)");
-    }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.printf("Input : ");
-        int num = sc.nextInt();
-        findEvenOdd(num);
-    }
-}

b6c551a9d2076bebf8ade204eac82ed6a9d96286
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-05-08 10:42:30 +0900

    Update pra1.java

--- src/session02/pra1.java
+++ src/session02/pra1.java
@@ -1,1 +1,24 @@
+package session02;
 
+class Calculator{
+    int avg(int[]  data) {
+        int total = 0;
+        for (int i = 0; i < data.length; i++) {
+            total += data[i];
+        }
+//        for(int num : data){
+//            total +=  num;
+//        }
+        return total /data.length;
+    }
+}
+public class pra1 {
+    public static void main(String[] args){
+        Calculator cal = new Calculator();
+
+        int[] data = {1,3,5,7,9,11};
+        int result = cal.avg(data);
+
+        System.out.println("avg : " + result);
+    }
+}

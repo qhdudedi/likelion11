f355b92282c8d751ec202ed9bf863233b1a579a6
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-06-05 00:44:54 +0900

    Create ProductService.java

---
+++ src/session/crudEx/service/ProductService.java
@@ -0,0 +1,20 @@
+package likelion.crudEx.service;
+
+import likelion.crudEx.domain.Product;
+import likelion.crudEx.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Transactional
+    public void saveProduct(Product product) {
+        productRepository.save(product);
+    }
+}

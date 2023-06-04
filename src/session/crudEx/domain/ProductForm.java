4336099171b9784b81dd07bc8e0138af94e4f92f
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-06-04 21:52:49 +0900

    Create ProductForm.java

---
+++ src/session/crudEx/domain/ProductForm.java
@@ -0,0 +1,12 @@
+package likelion.crudEx.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductForm {
+    private Long id;
+    private String name;
+    private int price;
+}
